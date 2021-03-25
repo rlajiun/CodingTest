52543c6a660a8e837dbd5d336a2123075a038b91
rlajiun <54028476+rlajiun@users.noreply.github.com>
2021-03-25 16:10:37 +0900

    Programmers Level 1

--- 연습문제/문자열 내림차순으로 배치하기.py
+++ 연습문제/문자열 내림차순으로 배치하기.py
@@ -1,2 +1,3 @@
 def solution(s):
-    return "".join(sorted(list(s), reverse=True))
+#     return "".join(sorted(list(s), reverse=True))
+    return "".join(sorted(s, reverse=True)) # sorted는 list로 변환 후 정렬하는 것이라서 list(s) 할 필요 없음
