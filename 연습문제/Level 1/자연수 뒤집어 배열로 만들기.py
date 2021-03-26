36eef369776d33634fa32527767a3879d1cdc7af
rlajiun <54028476+rlajiun@users.noreply.github.com>
2021-03-26 16:40:36 +0900

    Update 자연수 뒤집어 배열로 만들기.py

--- 연습문제/Level 1/자연수 뒤집어 배열로 만들기.py
+++ 연습문제/Level 1/자연수 뒤집어 배열로 만들기.py
@@ -1,5 +1,5 @@
 def solution(n):
-    answer = [int(i) for i in list(str(n))]
+    answer = [int(i) for i in list(str(n))] # list(str(n))은 그냥 str(n)으로 해도 list로 만들어짐
     answer.reverse()
 
     return answer
