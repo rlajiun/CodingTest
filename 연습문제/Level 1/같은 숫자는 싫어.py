f6f8a5b672205c0c46c000c636c6476db04cc9e0
rlajiun <54028476+rlajiun@users.noreply.github.com>
2021-03-28 19:59:06 +0900

    Update 같은 숫자는 싫어.py

--- 연습문제/Level 1/같은 숫자는 싫어.py
+++ 연습문제/Level 1/같은 숫자는 싫어.py
@@ -3,7 +3,7 @@ answer = []
 
 answer.append(arr[0])
 for a in arr:
-  if answer[-1]!=a:
+  if answer[-1]!=a: # answer[-1:] != [a] 로 하면 answer이 빈 리스트여도 오류가 발생하지 않음
     answer.append(a)
 
 print(answer) # [1,3,0,1]
