cd5d432a5b4ae466dc25f2fbb4fbe2a557766e02
rlajiun <54028476+rlajiun@users.noreply.github.com>
2021-05-06 15:10:50 +0900

    Create 타겟 넘버.py

---
+++ DFS_BFS/타겟 넘버.py
@@ -0,0 +1,17 @@
+from collections import deque
+def solution(numbers, target):
+    answer = 0
+    queue = deque([(0, 0)]) # sum, level
+    while queue:
+        s, l = queue.popleft()
+        if l > len(numbers):
+            break
+        elif l == len(numbers) and s == target:
+            answer += 1
+        queue.append((s+numbers[l-1], l+1))
+        queue.append((s-numbers[l-1], l+1))
+
+    return answer
+
+print(solution([1, 1, 1, 1, 1], 3))
+# https://programmers.co.kr/learn/courses/30/lessons/43165
